8917987f73016b239898579ff132c7d41c01ce04
Hanshi <hanshitrash00@proton.me>
2024-04-04 02:03:08 +0800

    refactor: Rewrite to vanilla js

--- main.js
+++ main.js
@@ -1,24 +1,80 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
+import Konva from "konva";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const WIDTH = window.innerWidth;
+const HEIGHT = window.innerHeight;
+let stagePos = { x: WIDTH / 2, y: HEIGHT / 2 };
+let groundLinePos = { x: 0, y: 0 };
+let prevPointerPos = { x: 0, y: 0 };
 
-setupCounter(document.querySelector('#counter'))
+const addObjectsOnLayer = (objectsLayer) => {
+	const ball = new Konva.Circle({
+		draggable: true,
+		x: -200,
+		y: groundLinePos.y - 25,
+		radius: 25,
+		stroke: "black",
+		strokeWidth: 1,
+	});
+	objectsLayer.add(ball);
+};
+
+const addGridsOnLayer = (gridLayer, stage) => {
+	const groundLine = new Konva.Line({
+		points: [-WIDTH, 0, WIDTH, 0],
+		x: groundLinePos.x,
+		y: groundLinePos.y,
+		stroke: "black",
+		strokeWidth: 1,
+	});
+	gridLayer.add(groundLine);
+	gridLayer.on("pointermove", () => {
+		prevPointerPos = stage.getPointerPosition();
+	});
+
+	gridLayer.on("dragmove", (e) => {
+		const currentPointerPosition = stage.getPointerPosition();
+		const offsetX = currentPointerPosition.x - prevPointerPos.x;
+		const offsetY = currentPointerPosition.y - prevPointerPos.y;
+
+		gridLayer.children.each((child) => {
+			const points = child.points();
+			child.points([
+				points[0] - offsetX,
+				points[1] - offsetY,
+				points[2] - offsetX,
+				points[3] - offsetY,
+			]);
+		});
+
+		prevPointerPos = currentPointerPosition;
+	});
+
+	gridLayer.on("mouseup touchend", () => {
+		isDragging = false;
+	});
+};
+
+const createStageAndLayers = () => {
+	const stage = new Konva.Stage({
+		container: "container",
+		width: WIDTH,
+		height: HEIGHT,
+		x: stagePos.x,
+		y: stagePos.y,
+	});
+
+	const gridLayer = new Konva.Layer();
+	const objectsLayer = new Konva.Layer();
+	stage.add(gridLayer, objectsLayer);
+	addGridsOnLayer(gridLayer);
+	addObjectsOnLayer(objectsLayer);
+};
+
+const runKonva = () => {
+	createStageAndLayers();
+};
+
+window.onload = () => {
+	runKonva();
+};
